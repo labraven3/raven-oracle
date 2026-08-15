ee5d22f79629da7d8618c6d35987eb801c69fc31
Sajid Malik <khansazid5071@gmail.com>
2026-08-16 03:21:39 +0530

    Fix raffle winner migration

--- prisma/migrations/20260816000000_fix_raffle_winner_selection_rank/migration.sql
+++ prisma/migrations/20260816000000_fix_raffle_winner_selection_rank/migration.sql
@@ -3,6 +3,8 @@
 
 DROP INDEX IF EXISTS "RaffleWinner_raffleId_selectionRank_key";
 
+DROP INDEX IF EXISTS "RaffleWinner_active_raffle_selection_rank_key";
+
 CREATE UNIQUE INDEX "RaffleWinner_active_raffle_selection_rank_key"
 ON "RaffleWinner" ("raffleId", "selectionRank")
 WHERE "status" IN ('SELECTED', 'NOTIFIED', 'CLAIMED');
